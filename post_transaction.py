a55f96d3c1c991ab3113fcc275ae3da934eb3a88
Rob Ward <wardrobert@gmail.com>
2019-11-03 18:37:03 +0000

    updated configparser for python 3 and added pandas for parsing csv amortization csv

--- post_transaction.py
+++ post_transaction.py
@@ -1,34 +1,43 @@
-import json
-import requests
-from ConfigParser import SafeConfigParser
+#!/usr/bin/python3.6
 
-parser = SafeConfigParser()
-parser.read('simple.ini')
+import sys
+import configparser
+import requests
+import pandas as pd
+from datetime import datetime, timedelta
+
+now = datetime.now()
+path = '/home/JBalloonist/ynab/'
+
+parser = configparser.ConfigParser()
+parser.read(path + 'simple.ini')
 TOKEN = parser.get('API', 'TOKEN')
 BUDGET_ID = parser.get('API', 'BUDGET_ID')
 ACCOUNT_ID = parser.get('API', 'ACCOUNT_ID')
 URL = 'https://api.youneedabudget.com/v1/'
 
-payload = {
-    "transaction": {
-        "account_id": "e51c95d2-209a-4a30-9384-09b91863dff7",
-        "date": "2019-11-01",
-        "amount": 1000,
-        "memo": "test",
-        "cleared": "cleared"
-    }
-}
+payload = {'transaction': {'date': '2019-10-21',
+  'payee': 'Union Savings Bank',
+  'amount': 226150,
+  'cleared': 'cleared',
+  'account_id': 'e51c95d2-209a-4a30-9384-09b91863dff7'}}
 
-# data={}
-# data['account_id'] = "d9d9c545-b754-49af-87ac-cc9991df9224"
-# data['date']="2019-10-23"
-# data['amount']=1000
-# data['memo'] ="test"
-# data['cleared']="cleared"
-# payload = {'transaction': data}
+df = pd.read_csv(path + 'loop.csv')
+df.date = pd.to_datetime(df.date)
+df.amount = df.amount *1000
+df.amount = df.amount.astype('int64')
 
-print(payload)
-
+plus_day = now + timedelta(days=2)
+minus_day = now - timedelta(days=2)
+print(minus_day)
+print(plus_day)
+trans1 = df.loc[(df.date < plus_day) & (df.date > minus_day)].copy()
+keep = df.loc[(df.date > plus_day)].copy()
+fmt = '%Y-%m-%d'
+trans1.date = trans1.date.dt.strftime(fmt)
+trans_dict = trans1.T.to_dict()
+trans1 = trans_dict[0]
+payload = {'transaction': trans1}
 
 url = '{}budgets/{}/transactions?access_token={}'.format(URL, BUDGET_ID, TOKEN)
 r = requests.post(url, json=payload)
@@ -38,3 +47,8 @@ print('')
 print(r.text)
 print('')
 print(r.request.body)
+
+keep.to_csv(path + 'loop.csv', index=False)
+
+
+

b58e80b4a865e8419c36d6d3e9d5eabdd274dc49
Rob Ward <wardrobert@gmail.com>
2019-11-16 15:22:51 +0000

    remove amortization function and add success check

--- post_transaction.py
+++ post_transaction.py
@@ -57,25 +57,7 @@ def post_trans(payload):
     print(r.text)
     print('')
     print(r.request.body)
-
-
-def amortization_parser(file, add_days, sub_days):
-    df = pd.read_csv(file)
-    df.date = pd.to_datetime(df.date)
-    df.amount = df.amount.astype('int64')
-
-    plus_day = now + timedelta(days=add_days)
-    minus_day = now - timedelta(days=sub_days)
-    print(minus_day)
-    print(plus_day)
-
-    keep = df.loc[(df.date > plus_day)].copy()
-    print('Data to keep from {}: '.format(file))
-    print(keep.head())
-    keep.to_csv(file, index=False)
-
-    trans = df.loc[(df.date < plus_day) & (df.date > minus_day)].copy()
-    return trans
+    return r.status_code
 
 
 def create_ynab_trans(trans):
@@ -83,19 +65,18 @@ def create_ynab_trans(trans):
     trans_dict = trans.T.to_dict()
     trans = trans_dict[0]
     payload = {'transaction': trans}
-    post_trans(payload)
+    return post_trans(payload)
 
-
-# create_ynab_trans(amortization_parser(PATH + 'loop.csv', 1, 1))
-# create_ynab_trans(amortization_parser(PATH + 'cudgel.csv', 1, 1))
 
 sheets = ['Cornerstone-{}.csv'.format(i) for i in range(1, 7)]
 sheets.pop(2)
+sheets = sheets + ['loop.csv', 'cudgel.csv']
+sheets = sheets + ['Navient-0{}.csv'.format(i) for i in range(1, 3)]
 
 for i in sheets:
     try:
-        cornerstone = Amortization(PATH + i, 2, 2)
-        create_ynab_trans(cornerstone.get_next())
-        # create_ynab_trans(amortization_parser(PATH + i, 1, 1))
+        parser = Amortization(PATH + i, 1, 2)
+        if str(create_ynab_trans(parser.get_next())) == '201':
+            parser.export_remaining()
     except:
         pass

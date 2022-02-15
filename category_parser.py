2516a0e56342d080641677b15ce0e39d1a2b5193
Rob Ward <wardrobert@gmail.com>
2022-02-14 23:10:28 -0500

    parsing broken down by master cat

--- category_parser.py
+++ category_parser.py
@@ -1,7 +1,10 @@
 import json
-# from collections import defaultdict
+from pathlib import Path
+from collections import defaultdict
 
-with open('/home/JBalloonist/ynab/data/categories.json', 'r') as out:
+PATH = Path.cwd()
+
+with open(PATH / 'data' / 'categories.json', 'r') as out:
     data = json.load(out)
 
 # print(json.dumps(data['data'], indent=4))
@@ -12,23 +15,33 @@ def remove_non_ascii(text):
     return ''.join(i for i in text if ord(i)<128)
 
 
-# category = []
-# balances = defaultdict(category)
-balances = []
+category = []
+balances = defaultdict(list)
+
 
 # update to add the main category and each of its balances as a dict
 for main_cat in category_groups:
     if main_cat['name'] in ['Needs', 'True Expenses', 'Debt and Taxes', 'Credit Card Payments', 'Quality of Life']:
         # print(json.dumps(main_cat['categories'], indent=4))
         categories = main_cat['categories']
+        master_cat = main_cat['name']
+        mast_balances = []
         for cat_data in categories:
             name = remove_non_ascii(cat_data['name'])
-            bal = cat_data['balance']
+            bal = cat_data['balance']            
             # print('{}: {}').format(name, bal)
 
             if bal < 0:
-                balances.append(bal)
+                # balances.append(bal)
+                mast_balances.append(bal)
+        
+        balances[master_cat] = mast_balances
 
-print(balances)
-overspent = sum(balances) / 1000
-print('Total overspent: {}').format(overspent)
+
+for k, v in balances.items():
+    print(k)
+    print(sum(v) / 1000)
+
+# print(balances)
+# overspent = sum(balances) / 1000
+# print('Total overspent: {}').format(overspent)
--- get_category.py
+++ get_category.py
@@ -1,17 +1,19 @@
 import json
 import requests
-from ConfigParser import SafeConfigParser
+import configparser
+from pathlib import Path
 
-parser = SafeConfigParser()
-parser.read('/home/JBalloonist/ynab/data/simple.ini')
+parser = configparser.ConfigParser()
+path = Path.home() / 'ynab' / 'data' / 'simple.ini'
+parser.read([path])
 TOKEN = parser.get('API', 'TOKEN')
 BUDGET_ID = parser.get('API', 'BUDGET_ID')
 URL = 'https://api.youneedabudget.com/v1/'
 
-month = '2020-02-01'
+month = '2022-02-01'
 category_id = '78bc9028-b0fb-4056-bc86-3b8ace523b92'
-# r = requests.get('{}budgets/{}/categories?access_token={}'.format(URL, BUDGET_ID, TOKEN))
-r = requests.get('{}budgets/{}/months/{}/categories/{}?access_token={}'.format(URL, BUDGET_ID, month, category_id, TOKEN))
+r = requests.get('{}budgets/{}/categories?access_token={}'.format(URL, BUDGET_ID, TOKEN))
+# r = requests.get('{}budgets/{}/months/{}/categories/{}?access_token={}'.format(URL, BUDGET_ID, month, category_id, TOKEN))
 
 print(r.status_code)
 data = r.json()
